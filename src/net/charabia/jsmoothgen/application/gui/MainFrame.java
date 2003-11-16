5893d50a48d2b87c395cd61d9aeafed8d284a6dd
Rodrigo Reyes <reyes@charabia.net>
2003-11-16 08:59:01 +0000

    set the system look-and-feel as the default

--- src/net/charabia/jsmoothgen/application/gui/MainFrame.java
+++ src/net/charabia/jsmoothgen/application/gui/MainFrame.java
@@ -638,6 +638,11 @@ public class MainFrame extends javax.swing.JFrame implements MainController
      */
     public static void main(String args[])
     {
+	try {
+	    UIManager.setLookAndFeel(
+				     UIManager.getSystemLookAndFeelClassName());
+	} catch (Exception e) { }
+	
 	new MainFrame().show();
     }
 	
