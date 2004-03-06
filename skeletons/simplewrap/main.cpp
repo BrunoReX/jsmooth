253b48e4fadb690675353bc3f0f858a5fe67c2d5
Rodrigo Reyes <reyes@charabia.net>
2004-03-06 13:31:52 +0000

    removed un-necessary window creation code, causing bug #858829

--- skeletons/simplewrap/main.cpp
+++ skeletons/simplewrap/main.cpp
@@ -22,23 +22,12 @@
 #include <iostream>
 
 #include "resource.h"
-
-
 #include "common.h"
 #include "ResourceManager.h"
 #include "JVMRegistryLookup.h"
 #include "JavaMachineManager.h"
 
-void SaveJarResource();
-
-/*  Declare Windows procedure  */
-LRESULT CALLBACK WindowProcedure (HWND, UINT, WPARAM, LPARAM);
-
-/*  Make the class name into a global variable  */
-char szClassName[ ] = "JSmooth";
-
 ResourceManager* globalResMan;
-
 DebugConsole *DEBUGCONSOLE = NULL;
 
 void lastExit()
@@ -48,15 +37,6 @@ void lastExit()
   delete globalResMan;
   globalResMan = 0;
 }
-
-// void debug(std::string text)
-// {
-// }
-
-// void _debugOutputCheck(std::string text)
-// {
-//   debug(text);
-// }
 
 void _debugOutput(const std::string& text)
 {
@@ -78,49 +58,8 @@ int WINAPI WinMain (HINSTANCE hThisInstance,
     HWND hwnd;               /* This is the handle for our window */
     MSG messages;            /* Here messages to the application are saved */
     WNDCLASSEX wincl;        /* Data structure for the windowclass */
-
-//    jlist->run();
-    
+   
     atexit(lastExit);
-    //    debugOutput = debug;
-    
-    /* The Window structure */
-    wincl.hInstance = hThisInstance;
-    wincl.lpszClassName = szClassName;
-    wincl.lpfnWndProc = WindowProcedure;      /* This function is called by windows */
-    wincl.style = CS_DBLCLKS | CS_HREDRAW | CS_VREDRAW;                 /* Catch double-clicks */
-    wincl.cbSize = sizeof (WNDCLASSEX);
-
-    /* Use default icon and mouse-pointer */
-    wincl.hIcon = LoadIcon (NULL, IDI_APPLICATION);
-    wincl.hIconSm = LoadIcon (NULL, IDI_APPLICATION);
-    wincl.hCursor = LoadCursor (NULL, IDC_ARROW);
-    wincl.lpszMenuName = NULL;                 /* No menu */
-    wincl.cbClsExtra = 0;                      /* No extra bytes after the window class */
-    wincl.cbWndExtra = 0;                      /* structure or the window instance */
-    /* Use Windows's default color as the background of the window */
-    wincl.hbrBackground = (HBRUSH) COLOR_BACKGROUND;
-
-    /* Register the window class, and if it fails quit the program */
-    if (!RegisterClassEx (&wincl))
-        return 0;
-
-    /* The class is registered, let's create the program*/
-
-    hwnd = CreateWindowEx (
-           0,                   /* Extended possibilites for variation */
-           szClassName,         /* Classname */
-           "Windows App",       /* Title Text */
-           WS_OVERLAPPEDWINDOW, /* default window */
-           CW_USEDEFAULT,       /* Windows decides the position */
-           CW_USEDEFAULT,       /* where the window ends up on the screen */
-           544,                 /* The programs width */
-           375,                 /* and height in pixels */
-           HWND_DESKTOP,        /* The window is a child-window to desktop */
-           NULL,                /* No menu */
-           hThisInstance,       /* Program Instance handler */
-           NULL                 /* No Window Creation data */
-           );
 
     globalResMan = new ResourceManager("JAVA", PROPID, JARID);
 
@@ -147,9 +86,6 @@ int WINAPI WinMain (HINSTANCE hThisInstance,
     string newcurdir = globalResMan->getProperty(ResourceManager::KEY_CURRENTDIR);
     SetCurrentDirectory(newcurdir.c_str());
 
-    ShowWindow(hwnd, SW_HIDE);
-    UpdateWindow(hwnd);
-
     JavaMachineManager man(*globalResMan);
     std::string preferDLLstr = globalResMan->getProperty("skel_SingleProcess");
 
@@ -166,61 +102,10 @@ int WINAPI WinMain (HINSTANCE hThisInstance,
         }
     }
 
-    //    MessageBox(hwnd, "Launched java", "DEBUG", MB_OKCANCEL|MB_ICONQUESTION|MB_APPLMODAL);
-
     DEBUG("NORMAL EXIT");
     DEBUGWAITKEY();
-
-    /* Make the window visible on the screen */
-
-//        ShowWindow (hwnd, nFunsterStil);
-//        UpdateWindow(hwnd);
-    /* Run the message loop. It will run until GetMessage() returns 0 */
-//     while (GetMessage (&messages, NULL, 0, 0) > 0)
-//     {
-//         /* Translate virtual-key messages into character messages */
-//         TranslateMessage(&messages);
-//         /* Send message to WindowProcedure */
-//         DispatchMessage(&messages);
-//     }
-
-//    MessageBox(hwnd, "AFTER GETMESSAGE", "DEBUG", MB_OKCANCEL|MB_ICONQUESTION|MB_APPLMODAL);
-
-    delete DEBUGCONSOLE;
 
     /* The program return-value is 0 - The value that PostQuitMessage() gave */
     return messages.wParam;
 }
 
-
-/*  This function is called by the Windows function DispatchMessage()  */
-
-LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    HDC hdc;
-	PAINTSTRUCT ps;
-
-    char buffer[255];    
-    char * mesg = "............................";
-    mesg = buffer;
-
-    switch (message)                  /* handle the messages */
-    {
-		case WM_PAINT:
-			hdc = BeginPaint(hwnd, &ps);
-			// TODO: Add any drawing code here...
-			RECT rt;
-			GetClientRect(hwnd, &rt);
-			DrawText(hdc, mesg, 10, &rt, DT_CENTER);
-			EndPaint(hwnd, &ps);
-			break;
-
-        case WM_DESTROY:
-            PostQuitMessage (0);       /* send a WM_QUIT to the message queue */
-            break;
-        default:                      /* for messages that we don't deal with */
-            return DefWindowProc (hwnd, message, wParam, lParam);
-    }
-
-    return 0;
-}
