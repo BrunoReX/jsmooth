b6b46735258a5cc9dbf5159266b2c4210eff939d
Rodrigo Reyes <reyes@charabia.net>
2007-04-07 10:08:21 +0000

    adds a destination dir parameter, as suggested and implemented by c_inconnu here: http://sourceforge.net/mailarchive/message.php?msg_name=20050920090245.99697.qmail%40web53103.mail.yahoo.com

--- ant/net/charabia/jsmooth/ant/JSmoothGen.java
+++ ant/net/charabia/jsmooth/ant/JSmoothGen.java
@@ -27,23 +27,56 @@ import java.io.*;
 
 public class JSmoothGen extends org.apache.tools.ant.Task
 {
+    /** JSmooth project file. */
     private java.io.File m_prjfile;
+
+    /** JSmooth skeletons root directory. */
     private java.io.File m_skeletonRoot;
-    private boolean verbose;
-    
+
+    /** Log level. */
+    private boolean m_verbose;
+
+    /** Destination directory for the generated executable. */
+    private File m_destdir;
+
+    /**
+     * Sets the destination directory for the generated executable.
+     *
+     * @param destdir the destination directory for the generated executable.
+     */
+    public void setDestdir(java.io.File destdir)
+    {
+        m_destdir = destdir;
+    }
+
+    /**
+     * Sets the JSmooth project file.
+     *
+     * @param prjfile the JSmooth project file.
+     */
     public void setProject(java.io.File prjfile)
     {
 	m_prjfile = prjfile;
     }
 
+    /**
+     * Sets the JSmooth skeletons root directory.
+     *
+     * @param skeletonRoot the JSmooth skeletons root directory.
+     */
     public void setSkeletonRoot(java.io.File skeletonRoot)
     {
 	m_skeletonRoot = skeletonRoot;
     }
 
+    /**
+     * Sets the log level to verbose or not.
+     *
+     * @param verbose the log level to verbose or not.
+     */
     public void setVerbose(boolean val) 
     {
-        verbose = val;
+        m_verbose = val;
     }
 
     public void execute() throws org.apache.tools.ant.BuildException
@@ -69,14 +102,24 @@ public class JSmoothGen extends org.apache.tools.ant.Task
 
 	    File basedir = prj.getParentFile();
 
+            File out;
+            if (m_destdir == null)
+            {
+                out = new File(basedir, model.getExecutableName());
+            }
+            else
+            {
+                out = new File(m_destdir, model.getExecutableName());
+            }
+            log("Executable file is " + out);
+
 	    SkeletonList skelList = new SkeletonList(m_skeletonRoot);
 
-	    File out = new File(basedir, model.getExecutableName());
 	    SkeletonBean skel = skelList.getSkeleton(model.getSkeletonName());
 	    File skelroot = skelList.getDirectory(skel);
 	    
 	    final ExeCompiler compiler = new ExeCompiler();
-	    if (verbose) {
+	    if (m_verbose) {
                 compiler.addListener(new ExeCompiler.StepListener() {
 			public void complete() {}
 			public void failed() {}
